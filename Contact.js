6916dd02b89ad17125d457ba49d87712dec2221e
WILDCATZWEB3 <dydxcryptocoin@gmail.com>
2025-05-16 01:16:58 +0100

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => (
+  <div>
+    <h2>Contact</h2>
+    <p>Email: contact@blockbeast.io</p>
+    <p>Twitter: @blockbeastnft</p>
+    <p>Telegram: @blockbeastcommunity</p>
+  </div>
+);
+
+export default Contact;
---
+++ Whitepaper.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Whitepaper = () => (
+  <div>
+    <h2>Whitepaper</h2>
+    <p>Your whitepaper content goes here. Explain your NFT utility, tokenomics, roadmap, and ecosystem.</p>
+  </div>
+);
+
+export default Whitepaper;
